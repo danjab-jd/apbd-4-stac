b1472cd43db7974971c2711f8b8e9451fce11456
danjab-jd <jd@pja.edu.pl>
2022-03-31 13:59:33 +0200

    Getting books from db

---
+++ APBD4/APBD4/Controllers/BooksController.cs
@@ -0,0 +1,34 @@
+﻿using APBD4.Models;
+using APBD4.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        // (Localdb)\MSSQLLocalDB
+
+        public BooksController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public async Task<IList<Book>> GetBookList(string orderBy)
+        {
+            //DbService db = new(); NIEDOBRE
+
+            return await _dbService.GetBooksListAsync();
+        }
+
+    }
+}
---
+++ APBD4/APBD4/Services/IDbService.cs
@@ -0,0 +1,15 @@
+﻿using APBD4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD4.Services
+{
+    public interface IDbService
+    {
+        Task<IList<Book>> GetBooksListAsync();
+
+        Task<Book> GetBookByTitle(string title);
+    }
+}
